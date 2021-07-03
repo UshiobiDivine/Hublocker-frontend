056f244cfa62021ade251ae7def800f0f46880f2
UshiobiDivine <divineobi13@gmail.com>
2021-07-03 20:12:33 +0100

    feat the table component

--- src/components/Table.js
+++ src/components/Table.js
@@ -7,6 +7,7 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
+import classes from "../css/Table.module.css";
 
 const useStyles = makeStyles({
   table: {
@@ -14,47 +15,89 @@ const useStyles = makeStyles({
   },
 });
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(size, price, firstPay, available, rent) {
+  return { size, price, firstPay, available, rent };
 }
 
 const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData(
+    "Small H295*W460*D520",
+    "N450 per item/mo N15,000 XX no of orders Online only price",
+    "1N For First Rent",
+    "2 Available",
+    "Rent Now"
+  ),
+  createData(
+    "Small H296*W460*D520",
+    "N450 per item/mo N15,000 XX no of orders Online only price",
+    "1N For First Rent",
+    "2 Available",
+    "Rent Now"
+  ),
+  createData(
+    "Small H297*W460*D520",
+    "N450 per item/mo N15,000 XX no of orders Online only price",
+    "1N For First Rent",
+    "2 Available",
+    "Rent Now"
+  ),
+  createData(
+    "Small H298*W460*D520",
+    "N450 per item/mo N15,000 XX no of orders Online only price",
+    "1N For First Rent",
+    "2 Available",
+    "Rent Now"
+  ),
+  createData(
+    "Small H299*W460*D520",
+    "N450 per item/mo N15,000 XX no of orders Online only price",
+    "1N For First Rent",
+    "2 Available",
+    "Rent Now"
+  ),
 ];
 
 export default function MyTable() {
-  const classes = useStyles();
+  //   const classes = useStyles();
 
   return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        {/* <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-          </TableRow>
-        </TableHead> */}
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <div>
+      {rows.map((row) => (
+        <div className={classes.container}>
+          <div className={classes.item}>{row.size}</div>
+          <div className={classes.item}>{row.price}</div>
+          <div className={classes.item}>{row.firstPay}</div>
+          <div className={classes.item}>{row.available}</div>
+          <div className={classes.rent}>{row.rent}</div>
+        </div>
+      ))}
+    </div>
+
+    // <TableContainer component={Paper}>
+    //   <Table className={classes.table} aria-label="simple table">
+    //     <TableHead>
+    //       <TableRow>
+    //         <TableCell>Dessert (100g serving)</TableCell>
+    //         <TableCell align="right">Calories</TableCell>
+    //         <TableCell align="right">Fat&nbsp;(g)</TableCell>
+    //         <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+    //         <TableCell align="right">Protein&nbsp;(g)</TableCell>
+    //       </TableRow>
+    //     </TableHead>
+    //     <TableBody>
+    //       {rows.map((row) => (
+    //         <TableRow key={row.name}>
+    //           <TableCell >
+    //             {row.name}
+    //           </TableCell>
+    //           <TableCell align="right">{row.calories}</TableCell>
+    //           <TableCell align="right">{row.fat}</TableCell>
+    //           <TableCell align="right">{row.carbs}</TableCell>
+    //           <TableCell align="right">{row.protein}</TableCell>
+    //         </TableRow>
+    //       ))}
+    //     </TableBody>
+    //   </Table>
+    // </TableContainer>
   );
 }
